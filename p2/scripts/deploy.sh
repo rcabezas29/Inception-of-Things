71956829841858d6278416ee363a397b2a25e3b6
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2024-02-17 12:54:19 +0100

    Remove k3s unusful configuration

--- p2/scripts/deploy.sh
+++ p2/scripts/deploy.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install -y curl
 
 # Install K3s
-curl -sfL https://get.k3s.io | K3S_TOKEN=password K3S_KUBECONFIG_MODE="644" sh -s - --node-ip "192.168.56.110"
+curl -sfL https://get.k3s.io | sh -
 
 # Configure k command
 echo 'alias k="sudo k3s kubectl"' >> /root/.bashrc
