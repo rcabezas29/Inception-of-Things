8cc9a6b02c62aa8f74c34bf567c156691f7781cd
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2024-02-17 12:49:45 +0100

    Fixed services networking - working deployment

--- p2/scripts/deploy.sh
+++ p2/scripts/deploy.sh
@@ -5,7 +5,11 @@ apt-get update
 apt-get install -y curl
 
 # Install K3s
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | K3S_TOKEN=password K3S_KUBECONFIG_MODE="644" sh -s - --node-ip "192.168.56.110"
+
+# Configure k command
+echo 'alias k="sudo k3s kubectl"' >> /root/.bashrc
+echo 'alias k="sudo k3s kubectl"' >> /home/vagrant/.bashrc
 
 # Deploy kubernetes infrastructure
 kubectl apply -f /vagrant/manifests/app-one.yaml
