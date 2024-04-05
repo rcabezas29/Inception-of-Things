fef6079bd0a002ddecfccf003e637e8ecb76936b
alvrodri <alvaromarianorodriguezbenitez@gmail.com>
2024-04-05 21:08:46 +0200

    finished

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -22,5 +22,6 @@ sudo kubectl -n argocd patch secret argocd-secret \
 # Deploy argocd application listener
 sudo kubectl apply -f /vagrant/manifests/application.yaml
 
-# Port forward argocd dashboard to port 8081
-nohup sudo kubectl port-forward svc/argocd-server --address="192.168.56.97" -n argocd 8081:80 &
+# Port forward argocd dashboard to port 8080 and wil's app to port 8888
+while true; do sudo kubectl port-forward svc/argocd-server -n argocd --address="0.0.0.0" 8080:443; done > /dev/null 2>&1 &
+while true; do sudo kubectl port-forward pod/$(sudo kubectl get pods -n dev | awk '{print $1}' | tail -1) -n dev --address="0.0.0.0" 8888:8888; done > /dev/null 2>&1 &
